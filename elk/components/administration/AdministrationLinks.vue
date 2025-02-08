6a661a9cfa8e5e7917470faaa038accab106c74d
Clovis <clovis@bdx.town>
2025-02-08 12:57:27 +0100

    feat: add admin panel

--- elk/components/administration/AdministrationLinks.vue
+++ elk/components/administration/AdministrationLinks.vue
@@ -13,7 +13,9 @@ function add() {
 }
 
 function save() {
-  update({ links: links.value }, isLoading)
+  const ls = links.value.filter(l => l.url && l.text)
+  update({ links: ls }, isLoading)
+  links.value = ls
 }
 
 function insertEmoji(url: string, emoji: string) {
@@ -26,65 +28,69 @@ function insertEmoji(url: string, emoji: string) {
 function onChange(e: Event, link: FrontendConfiguration['links'][number]) {
   const ls = [...links.value]
   const index = ls.findIndex(l => l.url === link.url)
-  const data = new FormData(e.currentTarget as HTMLFormElement)
-  ls.splice(index, 1, { ...ls[index], text: data.get('text') as string, url: data.get('url') as string })
+  const data = new FormData((e.currentTarget as HTMLFieldSetElement).form!)
+  ls.splice(index, 1, { ...ls[index], text: data.get(`${link.url}-text`) as string, url: data.get(`${link.url}-url`) as string })
   links.value = ls
 }
 </script>
 
 <template>
-  <div>
+  <form @submit.prevent="save">
+    <h2 font-medium>
+      {{ $t('admin.links.title') }}
+    </h2>
+    <p my-2>
+      {{ $t('admin.links.description') }}
+    </p>
     <div flex flex-col gap-3>
       <template v-for="link in links" :key="link.url">
         <div flex items-center gap-3>
           <EmojiPicker :hide-custom-emojis="true" @select="(emoji) => insertEmoji(link.url, emoji)">
-            <button grayscale input-base w-auto h-full>
+            <button grayscale input-base w-auto h-full type="button">
               {{ link.icon }}
             </button>
           </EmojiPicker>
-          <form grow flex items-center gap-3 @input="(e) => onChange(e, link)">
+          <fieldset grow flex items-center gap-3 @change="(e) => onChange(e, link)">
             <input
               :value="link.text"
-              name="text"
+              :name="`${link.url}-text`"
               shrink-1
               type="text" placeholder-text-secondary
-              :placeholder="$t('settings.profile.appearance.profile_metadata_label')"
+              :placeholder="$t('admin.links.label')"
               input-base
               w-auto
             >
             <input
               :value="link.url"
-              name="url"
+              :name="`${link.url}-url`"
               grow
               type="text" placeholder-text-secondary
-              :placeholder="$t('settings.profile.appearance.profile_metadata_value')"
+              :placeholder="$t('admin.links.url')"
               w-auto
               input-base
+              pattern="https?://.*"
             >
-          </form>
+          </fieldset>
         </div>
       </template>
       <button
         btn-outline font-bold py2 full-w sm-wa flex="~ gap2 center"
         type="button"
-        :disabled="!!links.find(l => !l.url || !l.text)"
-        :class="!!links.find(l => !l.url || !l.text) ? 'border-none' : undefined"
         @click="add"
       >
         <span aria-hidden="true" class="block i-ri:add-line" />
-        {{ $t('admin.add_link') }}
+        {{ $t('admin.links.add') }}
       </button>
     </div>
     <button
       btn-outline font-bold py2 full-w sm-wa flex="~ gap2 center" ml-auto mt-3
-      type="button"
+      type="submit"
       :disabled="isLoading"
       :class="isLoading ? 'border-none' : undefined"
-      @click="save"
     >
-      <span v-if="!isLoading" aria-hidden="true" class="block i-ri:add-line" />
+      <span v-if="!isLoading" aria-hidden="true" class="block i-ri:save-2-line" />
       <span v-else i-ri:loader-4-line animate-spin />
       {{ $t('action.save') }}
     </button>
-  </div>
+  </form>
 </template>
